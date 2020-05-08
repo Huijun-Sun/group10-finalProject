912a76b6f8e2fac54c8c8deb82e400df148bd9e4
anujaphadnis14 <63469171+anujaphadnis14@users.noreply.github.com>
2020-05-08 12:42:38 -0400

    Update index.js

--- data/index.js
+++ data/index.js
@@ -1,4 +1,5 @@
 const universityData = require('./universities');  
 const DiscussionData = require('./Discussion');  
+const userProfileData = require('./userprofile'); 
 
-module.exports = {  universities: universityData,Discussion: DiscussionData};
+module.exports = {  universities: universityData,Discussion: DiscussionData,userprofile: userProfileData};
