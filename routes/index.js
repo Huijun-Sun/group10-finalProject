4b36492774668209daca4ccd72b3c560455dc667
anujaphadnis14 <63469171+anujaphadnis14@users.noreply.github.com>
2020-05-09 13:27:16 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,14 @@ const DcRoutes = require("./DiscussionComment");
 const userproRoutes = require("./userprofile");
 const introRoutes = require("./intro")
 const eventsRoutes = require("./events")
+const userRoutes = require("./user");
 
 const constructorMethod = app => {
   app.use("/", introRoutes);
   app.use("/events", eventsRoutes);
-
-
   app.use("/universities", universitiesRoutes);
   app.use("/userprofile", userproRoutes);
- 
+  app.use("/user", userRoutes);
   app.use("/DiscussionTopic", DtRoutes);
   app.use("/DiscussionComment", DcRoutes);
   app.use("*", (req, res) => {
